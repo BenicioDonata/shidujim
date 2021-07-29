9bdfc585064fafa398e59dd4c703c26d50564d6d
unknown <manuelcontrera@gmail.com>
2021-07-29 19:31:37 -0300

    formatear foto y comentarios

--- resources/views/partials/forms/sections_edit/step6.blade.php
+++ resources/views/partials/forms/sections_edit/step6.blade.php
@@ -2,12 +2,16 @@
     <div class="row bg-dark">
         <div class="form-group col-md-6">
             <label for="files"><i class="far fa-check-circle"></i> Fotos Subidas </label>
+        </div>
+        <div class="form-group col-md-6 text-center">
+        </div>
+        @php
+            @endphp
             @foreach($form['form']['files'] as $file)
-                <div class="form-group col-md-12">
-                    <img  src="images/upload_forms/{{$file->files_name}}" alt="{{$file->files_name}}" width="100" height="100">
+                <div class="form-group col-md-6 text-center border">
+                    <img  src="/registro/images/upload_forms/{{$file->files_name}}" alt="{{$file->files_name}}" width="300" height="300">
                 </div>
             @endforeach
-        </div>
     </div>
 
 </div>
--- resources/views/partials/forms/sections_edit/step7.blade.php
+++ resources/views/partials/forms/sections_edit/step7.blade.php
@@ -2,12 +2,18 @@
     <div class="row bg-dark">
         <div class="form-group col-md-6">
             <label for="about_u"><i class="far fa-check-circle"></i> Comentarios </label>
-            @foreach($form['form']['comments'] as $comment)
+            @if(count($form['form']['comments']) == 0)
                 <div class="form-group col-md-12">
-                    <label><b>{{$comment->user->name}} comento el {{$comment->created_at}}</b></label><br/>
-                    <label>{{$comment->comment}}</label>
+                    <label><b>No hay comentarios para este postulante</b></label><br/>
                 </div>
-            @endforeach
+            @else
+                @foreach($form['form']['comments'] as $comment)
+                    <div class="form-group col-md-12">
+                        <label><b>{{$comment->user->name}} comento el {{$comment->created_at}}</b></label><br/>
+                        <label>{{$comment->comment}}</label>
+                    </div>
+                @endforeach
+            @endif
         </div>
     </div>
 
